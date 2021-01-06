f519727cb081403c7f88174d5c123b29e57a6e8b
Angelica <atejeda@espol.edu.ec>
2021-01-05 22:56:29 -0500

    actualizando barra navegacion

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -3,21 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SideBarComponent } from './components/shared/vista-lectura/side-bar/side-bar.component';
 import { ToolBarComponent } from './components/shared/vista-lectura/tool-bar/tool-bar.component';
+import { HomeComponent } from './components/shared/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     NavbarComponent,
     SideBarComponent,
-    ToolBarComponent
+    ToolBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
