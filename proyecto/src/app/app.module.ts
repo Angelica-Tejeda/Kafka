a782c2aed4768a6dca6091c8ed0d24674dafa1bf
Angelica <atejeda@espol.edu.ec>
2021-01-07 02:12:49 -0500

    creando vista de lectura

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -5,10 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { SideBarComponent } from './components/shared/vista-lectura/side-bar/side-bar.component';
-import { ToolBarComponent } from './components/shared/vista-lectura/tool-bar/tool-bar.component';
 import { HomeComponent } from './components/shared/home/home.component';
-import { TextViewEditorComponent } from './components/shared/vista-lectura/text-view-editor/text-view-editor.component';
 import { VistaLecturaComponent } from './components/shared/vista-lectura/vista-lectura.component';
 
 
@@ -17,10 +14,7 @@ import { VistaLecturaComponent } from './components/shared/vista-lectura/vista-l
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    SideBarComponent,
-    ToolBarComponent,
     HomeComponent,
-    TextViewEditorComponent,
     VistaLecturaComponent
   ],
   imports: [
