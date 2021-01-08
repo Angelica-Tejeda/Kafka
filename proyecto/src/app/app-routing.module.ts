9a5fdae985a52783854c27d6ba4baf1e8ec7fbac
keanceva <keanceva@espol.edu.ec>
2021-01-08 08:58:13 -0500

    Vista escritura

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/shared/home/home.component';
 import { VistaLecturaComponent } from './components/shared/vista-lectura/vista-lectura.component';
+import { VistaEscrituraComponent } from './components/shared/vista-escritor/vista-escritura.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'homeLector',             component: HomeComponent },
   { path: 'homeEscritor',           component: HomeComponent },
-  { path: 'vistaLectura',     component: VistaLecturaComponent }
+  { path: 'vistaLectura',     component: VistaLecturaComponent },
+  { path: 'vistaEscritura',     component: VistaEscrituraComponent }
 ];
 
 @NgModule({
