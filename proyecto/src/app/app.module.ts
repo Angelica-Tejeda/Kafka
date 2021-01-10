e673532cb3b395d8a7dc2fadf1d6d233795ccbd0
Víctor Moyano <vjmoyano@espol.edu.ec>
2021-01-09 21:12:07 -0500

    formulario libro, servicio libro

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
+import {FormlibroComponent} from './components/formlibro/formlibro.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
@@ -8,7 +10,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/shared/home/home.component';
 import { VistaLecturaComponent } from './components/shared/vista-lectura/vista-lectura.component';
 import { VistaEscrituraComponent } from './components/shared/vista-escritor/vista-escritura.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {LibroService} from './services/libro.service'
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,13 +21,17 @@ import { VistaEscrituraComponent } from './components/shared/vista-escritor/vist
     NavbarComponent,
     HomeComponent,
     VistaLecturaComponent,
-    VistaEscrituraComponent
+    VistaEscrituraComponent,
+    FormlibroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LibroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
