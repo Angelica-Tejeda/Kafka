e5ebb82bab9b22fe664be1982612f0ee863ece5e
keanceva <keanceva@espol.edu.ec>
2021-01-23 01:15:18 -0500

    cambio

--- proyecto/src/app/components/shared/vista-escritor/vista-escritura.component.ts
+++ proyecto/src/app/components/shared/vista-escritor/vista-escritura.component.ts
@@ -15,5 +15,39 @@ export class VistaEscrituraComponent implements OnInit {
   atras(){
     this.location.back();
   }
+  /*
+  prueba(){
+    let output = document.getElementById('output');
+    let buttons: any = document.getElementsByClassName('tool--btn');
+    //for(var _i = 0; _i < buttons.length; _i++){
+    for (let btn of buttons) {
+      btn.addEventListener('click', () => {
+        let cmd = btn.dataset['command'];
+        if(cmd === 'createlink') {
+          let url = prompt("Enter the link here: ", "http:\/\/");
+          document.execCommand(cmd, false, url);
+        } else {
+          document.execCommand(cmd, false, null);
+        }
+      })
+    }
+    buttons.forEach(btn => {
+      
+    });
+  }*/
 
 }
+/*
+let output = document.getElementById('output');
+let buttons = document.getElementsByClassName('tool--btn');
+for (let btn of buttons) {
+	btn.addEventListener('click', () => {
+		let cmd = btn.dataset['command'];
+		if(cmd === 'createlink') {
+			let url = prompt("Enter the link here: ", "http:\/\/");
+			document.execCommand(cmd, false, url);
+		} else {
+			document.execCommand(cmd, false, null);
+		}
+	})
+}*/
