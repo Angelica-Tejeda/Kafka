8bddf69a3ccbe5e12f85e70d0fc59495ae52c2d9
josalcab <josalcab@espol.edu.ec>
2021-06-02 17:49:36 -0500

    correcciones menores y script de base de datos subido

--- proyecto/src/app/components/collection/collection.component.ts
+++ proyecto/src/app/components/collection/collection.component.ts
@@ -9,7 +9,7 @@ import { Libro } from './libro.model';
   styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
-  coleccion: Libro[] = [new Libro("Harry Potter y el cáliz de fuego","Literatura fantástica","Mayores de 12 años","../../../assets/img/goblet.jpg"), new Libro("El resplandor","Terror","Mayores de 18 años","../../../assets/img/resplandor.jpg"), new Libro("Contacto","Ciencia ficción","Mayores de 12 años","../../../assets/img/contacto.jpg"), new Libro("La Metamorfosis","Ciencia Ficcion","Todo público","../../../assets/img/metamorfosisPortada.jpg")];
+  coleccion: Libro[] = [new Libro("Harry Potter y el cáliz de fuego","J.K Rowling","Mayores de 12 años","../../../assets/img/goblet.jpg"), new Libro("El resplandor","Stephen King","Mayores de 18 años","../../../assets/img/resplandor.jpg"), new Libro("Contacto","Carl Sagan","Mayores de 12 años","../../../assets/img/contacto.jpg"), new Libro("La Metamorfosis","Franz Kafka","Todo público","../../../assets/img/metamorfosisPortada.jpg"), new Libro("Drácula","Bram Stoker","Mayores de 12 años","../../../assets/img/dracula.jpg")];
 
   constructor(private router: Router, private libros:LibroService) {
 
@@ -19,8 +19,8 @@ export class CollectionComponent implements OnInit {
     this.libros.getCollection().subscribe((res:any)=>{
       if(res){
         for(let libro of res){
-          console.log(libro.titulo,libro.genero,libro.clasificacion);
-          this.coleccion.push(new Libro(libro.titulo,libro.genero,libro.clasificacion,libro.imgSource))
+          console.log(libro.titulo,libro.autor,libro.clasificacion);
+          this.coleccion.push(new Libro(libro.titulo,libro.autor,libro.clasificacion,libro.imgSource))
         }
       }
 
